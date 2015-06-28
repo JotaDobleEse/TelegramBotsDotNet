06ec947ce7777e46dd029e762424f486db7561b5
tchusami <tchusami_25@hotmail.com>
2015-06-29 01:00:16 +0200

    Implementando envio de fotos

--- TelegramBotsAPI/BotApi.cs
+++ TelegramBotsAPI/BotApi.cs
@@ -16,14 +16,9 @@ namespace TelegramBotsAPI
     {
         public string Token { get; private set; }
         public string BaseUrl { get { return "https://api.telegram.org/bot" + Token + "/"; } }
-        private BotApi(string token)
+        public BotApi(string token)
         {
             Token = token;
-        }
-
-        public static BotApi GetInstance(string token)
-        {
-            return new BotApi(token);
         }
 
         /// <summary>
@@ -70,7 +65,6 @@ namespace TelegramBotsAPI
             request.Method = "POST";
 
             var response = (HttpWebResponse)request.GetResponse();
-
             var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
             JObject json = JObject.Parse(responseString);
             Console.WriteLine(json.ToString());
@@ -98,7 +92,7 @@ namespace TelegramBotsAPI
         /// <param name="reply_to_message_id">If the message is a reply, ID of the original message.</param>
         /// <param name="reply_markup">Additional interface options. A JSON-serialized object for a custom reply keyboard, instructions to hide keyboard or to force a reply from the user.</param>
         /// <returns></returns>
-        public Message SendMessage2(int chat_id, string text, bool disable_web_page_preview = false, int reply_to_message_id = -1, ReplyKeyboardMarkup reply_markup = null)
+        public Message SendMessageAndShowKeyboard(int chat_id, string text, ReplyKeyboardMarkup reply_markup, bool disable_web_page_preview = false, int reply_to_message_id = -1)
         {
             string url = BaseUrl + "sendMessage?text=" + text + "&chat_id=" + chat_id;
             if (disable_web_page_preview)
@@ -114,7 +108,6 @@ namespace TelegramBotsAPI
             request.Method = "POST";
 
             var response = (HttpWebResponse)request.GetResponse();
-
             var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
             JObject json = JObject.Parse(responseString);
             Console.WriteLine(json.ToString());
@@ -143,7 +136,7 @@ namespace TelegramBotsAPI
         /// <param name="reply_to_message_id">If the message is a reply, ID of the original message.</param>
         /// <param name="reply_markup">Additional interface options. A JSON-serialized object for a custom reply keyboard, instructions to hide keyboard or to force a reply from the user.</param>
         /// <returns></returns>
-        public Message SendMessage3(int chat_id, string text, bool disable_web_page_preview = false, int reply_to_message_id = -1, ReplyKeyboardHide reply_markup = null)
+        public Message SendMessageAndHideKeyboard(int chat_id, string text, ReplyKeyboardHide reply_markup, bool disable_web_page_preview = false, int reply_to_message_id = -1)
         {
             string url = BaseUrl + "sendMessage?text=" + text + "&chat_id=" + chat_id;
             if (disable_web_page_preview)
@@ -159,7 +152,6 @@ namespace TelegramBotsAPI
             request.Method = "POST";
 
             var response = (HttpWebResponse)request.GetResponse();
-
             var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
             JObject json = JObject.Parse(responseString);
             Console.WriteLine(json.ToString());
@@ -188,9 +180,160 @@ namespace TelegramBotsAPI
         /// <param name="reply_to_message_id">If the message is a reply, ID of the original message.</param>
         /// <param name="reply_markup">Additional interface options. A JSON-serialized object for a custom reply keyboard, instructions to hide keyboard or to force a reply from the user.</param>
         /// <returns></returns>
-        public Message SendMessage4(int chat_id, string text, bool disable_web_page_preview = false, int reply_to_message_id = -1, ForceReply reply_markup = null)
+        public Message SendMessageAndForceReply(int chat_id, string text, ForceReply reply_markup, bool disable_web_page_preview = false, int reply_to_message_id = -1)
         {
+            string url = BaseUrl + "sendMessage?text=" + text + "&chat_id=" + chat_id;
+            if (disable_web_page_preview)
+                url += "&disable_web_page_preview=true";
+            if (reply_to_message_id != -1)
+                url += "&reply_to_message_id=" + reply_to_message_id;
+            if (reply_markup != null)
+                url += "&reply_markup=" + JsonConvert.SerializeObject(reply_markup, Formatting.None);
+
+            Console.WriteLine(url);
+
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+
+            var response = (HttpWebResponse)request.GetResponse();
+            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+            JObject json = JObject.Parse(responseString);
+            Console.WriteLine(json.ToString());
+            if (json.GetValue("ok").ToString().ToLower() == "true")
+            {
+                try
+                {
+                    Message message = JsonConvert.DeserializeObject<Message>(json.GetValue("result").ToString());
+                    return message;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
             return null;
         }
+
+        /// <summary>
+        /// Use this method to forward messages of any kind. On success, the sent Message is returned.
+        /// </summary>
+        /// <param name="chat_id">Unique identifier for the message recipient — User or GroupChat id.</param>
+        /// <param name="from_chat_id">Unique identifier for the chat where the original message was sent — User or GroupChat id.</param>
+        /// <param name="message_id">Unique message identifier</param>
+        /// <returns></returns>
+        public Message ForwardMessage(int chat_id, int from_chat_id, int message_id)
+        {
+            string url = BaseUrl + "forwardMessage?chat_id=" + chat_id + "&from_chat_id=" + from_chat_id + "&message_id=" + message_id;
+
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+
+            var response = (HttpWebResponse)request.GetResponse();
+            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+            JObject json = JObject.Parse(responseString);
+            Console.WriteLine(json.ToString());
+            if (json.GetValue("ok").ToString().ToLower() == "true")
+            {
+                try
+                {
+                    Message message = JsonConvert.DeserializeObject<Message>(json.GetValue("result").ToString());
+                    return message;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Use this method to send photos. On success, the sent Message is returned.
+        /// </summary>
+        /// <param name="chat_id">Unique identifier for the message recipient — User or GroupChat id.</param>
+        /// <param name="photo_path">Photo to send. You can upload a new photo using multipart/form-data.</param>
+        /// <param name="caption">Photo caption (may also be used when resending photos by file_id).</param>
+        /// <param name="reply_to_message_id">If the message is a reply, ID of the original message.</param>
+        /// <returns></returns>
+        public Message SendPhoto(int chat_id, string photo_path, string caption = null, int reply_to_message_id = -1)
+        {
+            string url = BaseUrl + "sendPhoto?chat_id=" + chat_id;// +"&photo=" + photo_path;
+            if (!string.IsNullOrEmpty(caption))
+                url += "&caption=" + caption;
+            if (reply_to_message_id != -1)
+                url += "&reply_to_message_id=" + reply_to_message_id;
+
+            byte[] file = File.ReadAllBytes(photo_path);
+
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.Headers.Add("Enctype: multipart/form-data");
+            request.Method = "POST";
+
+
+            using(Stream writer = request.GetRequestStream())
+            {
+                byte[] encode = Encoding.UTF8.GetBytes("photo=");
+                writer.Write(encode, 0, encode.Length);
+                writer.Write(file, 0, file.Length);
+            }
+
+            var response = (HttpWebResponse)request.GetResponse();
+            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+            JObject json = JObject.Parse(responseString);
+            Console.WriteLine(json.ToString());
+            if (json.GetValue("ok").ToString().ToLower() == "true")
+            {
+                try
+                {
+                    Message message = JsonConvert.DeserializeObject<Message>(json.GetValue("result").ToString());
+                    return message;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Use this method to send photos. On success, the sent Message is returned.
+        /// </summary>
+        /// <param name="chat_id">Unique identifier for the message recipient — User or GroupChat id.</param>
+        /// <param name="photo_id">Photo to send. You can pass a file_id as String to resend a photo that is already on the Telegram servers.</param>
+        /// <param name="caption">Photo caption (may also be used when resending photos by file_id).</param>
+        /// <param name="reply_to_message_id">If the message is a reply, ID of the original message.</param>
+        /// <returns></returns>
+        public Message ResendPhoto(int chat_id, string photo_id, string caption = null, int reply_to_message_id = -1)
+        {
+            string url = BaseUrl + "sendPhoto?chat_id=" + chat_id + "&photo=" + photo_id;
+            if (!string.IsNullOrEmpty(caption))
+                url += "&caption=" + caption;
+            if (reply_to_message_id != -1)
+                url += "&reply_to_message_id=" + reply_to_message_id;
+
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+
+            var response = (HttpWebResponse)request.GetResponse();
+            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+            JObject json = JObject.Parse(responseString);
+            Console.WriteLine(json.ToString());
+            if (json.GetValue("ok").ToString().ToLower() == "true")
+            {
+                try
+                {
+                    Message message = JsonConvert.DeserializeObject<Message>(json.GetValue("result").ToString());
+                    return message;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            return null;
+        }
+
+
     }
 }
